8b260e5e62dfb61be59eb7562217d15b0a525073
fctopz <38725882+fctopz@users.noreply.github.com>
2023-10-27 08:00:10 +0000

    bootcamp day1

---
+++ hello_solana/target/debug/deps/hello_solana-ab45cc3415ec2e96.d
@@ -0,0 +1,7 @@
+/workspaces/sbc-workspace/hello_solana/target/debug/deps/hello_solana-ab45cc3415ec2e96.rmeta: src/lib.rs
+
+/workspaces/sbc-workspace/hello_solana/target/debug/deps/libhello_solana-ab45cc3415ec2e96.rlib: src/lib.rs
+
+/workspaces/sbc-workspace/hello_solana/target/debug/deps/hello_solana-ab45cc3415ec2e96.d: src/lib.rs
+
+src/lib.rs:
